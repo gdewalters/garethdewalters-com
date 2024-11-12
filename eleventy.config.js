8a55265a45a2bdeed9f3a3e918f00040f2843f05
gdewalters <gareth@dewalters.net>
2024-11-12 22:32:01 +1300

    Add nvmrc

--- eleventy.config.js
+++ eleventy.config.js
@@ -173,6 +173,4 @@ module.exports = async function(eleventyConfig) {
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
 
-
-
 };
