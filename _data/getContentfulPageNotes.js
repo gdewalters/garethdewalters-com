78fc0de6aa06f37c61a34d11e3cd61c6b35f8dd7
gdewalters <gareth@dewalters.net>
2025-08-31 18:24:49 +1200

    Adding new notes section

---
+++ _data/getContentfulPageNotes.js
@@ -0,0 +1,8 @@
+//  getContentfulPageNotes.js
+// This module fetches the writing page from Contentful by its slug and processes it.            
+
+import { getContentfulPageBySlug } from './getContentfulPageBySlug.js';
+
+export default async function () {
+  return getContentfulPageBySlug('notes');
+}
